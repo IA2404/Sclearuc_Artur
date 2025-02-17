32d6a9d44c703cc688654df28f603676856365ad
Artur <zerq.arkn@mail.ru>
2025-02-17 17:13:07 +0200

    Add files via upload

---
+++ lab1/script.js
@@ -0,0 +1,23 @@
+alert("Этот код выполнен из внешнего файла!");
+console.log("Сообщение в консоли");
+
+let name = "Артур";
+let b_year = "2005";
+let student = true;
+
+console.log("Имя,", name);
+console.log("Год рождения:", b_year);
+console.log("Учится?:", student);
+
+let score = prompt("Введите ваш балл:");
+if (score >= 90) {
+ console.log("Отлично!");
+} else if (score >= 70) {
+ console.log("Хорошо");
+} else {
+ console.log("Можно лучше!");
+}
+
+for (let i = 1; i <= 5; i++) {
+ console.log(`Итерация: ${i}`);
+}
